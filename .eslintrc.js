4ccfd7c3bc14efe7c9b673ad700d20062d9628e0
Unyielding1 <david.verde.alvarez@gmail.com>
2022-05-19 14:33:56 -0500

    linters fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        "ecmaVersion": "latest"
     },
     "rules": {
     }
